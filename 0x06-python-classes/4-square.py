b613944d60c13ddb05ed92cf4fe7fc3cb96463ba
Vickibe1 <victoriaiberumu@gmail.com>
2023-03-29 15:39:43 -0700

    task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
+# 4-square.py
 """Define a class Square."""
 
 
 class Square:
     """Represent a square."""
-
 
     def __init__(self, size=0):
         """Initialize a new square.
